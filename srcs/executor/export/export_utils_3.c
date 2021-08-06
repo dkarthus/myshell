fbe77cbf593fbaf3e8f065a922f567417ed7eae6
Waves Anisha <itonyluke@gmail.com>
2021-08-06 13:40:06 +0300

    some changes to export

--- srcs/executor/export/export_utils_3.c
+++ srcs/executor/export/export_utils_3.c
@@ -10,6 +10,8 @@ int	if_for_check_key(char *str)
 		return (2);
 	else if (str[0] == '_' && str[1] == '=' && str[2] == '\0')
 		return (2);
+	else if (str[0] == '_' && str[1] != '\0' && ft_isalpha(str[1]))
+		return (2);
 	else if (str[0] == '~' && str[1] == '\0')
 		return (3);
 	else if (str[0] == '~' && str[1] == '/' && str[2] == '\0')
