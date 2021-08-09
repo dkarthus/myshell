90bf5d187fa73777db9f7330f2a8500321db64f9
Waves Anisha <itonyluke@gmail.com>
2021-08-09 12:19:07 +0300

    deleted unneccessary files

---
+++ srcs/test.c
@@ -0,0 +1,5 @@
+//
+// Created by Waves Anisha on 8/9/21.
+//
+
+#include "test.h"
---
+++ srcs/test.h
@@ -0,0 +1,8 @@
+//
+// Created by Waves Anisha on 8/9/21.
+//
+
+#ifndef MYSHELL_TEST_H
+#define MYSHELL_TEST_H
+
+#endif //MYSHELL_TEST_H
